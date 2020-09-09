829146869fa0ee759bd1db7ff7d55950618d6ffb
Torrmai <chanawatkuku@gmail.com>
2020-09-09 09:02:12 +0700

    first commit la

---
+++ build/.main.o.d
@@ -0,0 +1,163 @@
+dep_main.o = /home/chanawat/project/main.c /usr/include/stdc-predef.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_config.h \
+ /usr/include/time.h /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stddef.h \
+ /usr/include/bits/time.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/timex.h \
+ /usr/include/xlocale.h /usr/include/signal.h /usr/include/bits/sigset.h \
+ /usr/include/bits/signum.h /usr/include/bits/siginfo.h \
+ /usr/include/bits/sigaction.h /usr/include/bits/sigcontext.h \
+ /usr/include/bits/sigstack.h /usr/include/sys/ucontext.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/sigthread.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/wchar.h /usr/include/inttypes.h \
+ /usr/include/getopt.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_eal.h \
+ /usr/include/sched.h /usr/include/bits/sched.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_config.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_compat.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_per_lcore.h \
+ /usr/include/pthread.h /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/byteswap-16.h \
+ /usr/include/bits/setjmp.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_bus.h \
+ /usr/include/stdio.h /usr/include/libio.h /usr/include/_G_config.h \
+ /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdarg.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h /usr/include/sys/queue.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_log.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdbool.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_common.h \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/sys/types.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/sys/sysmacros.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h /usr/include/ctype.h \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/limits.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /home/chanawat/dpdk-20.05/lib/librte_eal/linux/include/rte_os.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_dev.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_pci_dev_feature_defs.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ethdev.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_interrupts.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_eal_interrupts.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_devargs.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_errno.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ether.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_memcpy.h \
+ /usr/include/string.h /usr/include/bits/string.h \
+ /usr/include/bits/string2.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_vect.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/generic/rte_vect.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/x86intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/ia32intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/mm_malloc.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/pmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/tmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/smmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/popcntintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/wmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/immintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/avxintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/avx2intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/lzcntintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/bmiintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/bmi2intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/fmaintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/f16cintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/rtmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xtestintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/rdseedintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/prfchwintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/fxsrintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xsaveintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xsaveoptintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/adxintrin.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_random.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_mbuf.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_mempool.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_spinlock.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/generic/rte_spinlock.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_lcore.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_launch.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_pause.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/generic/rte_pause.h \
+ /usr/include/assert.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_atomic.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/generic/rte_atomic.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_atomic_64.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_rtm.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_cpuflags.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/generic/rte_cpuflags.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_branch_prediction.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_common.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_pause.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_cycles.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/generic/rte_cycles.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_debug.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_log.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_branch_prediction.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_memory.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_fbarray.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_rwlock.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/generic/rte_rwlock.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_spinlock.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ring.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ring_core.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_memzone.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ring_generic.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ring_elem.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ring_hts.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ring_hts_c11_mem.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ring_rts.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ring_rts_c11_mem.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ring_peek.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ring_peek_c11_mem.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_mempool_trace_fp.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_trace_point.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_cycles.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_string_fns.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_uuid.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_prefetch.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/generic/rte_prefetch.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_byteorder.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/generic/rte_byteorder.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_byteorder_64.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_mbuf_ptype.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_mbuf_core.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/generic/rte_atomic.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ethdev_trace_fp.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_dev_info.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_eth_ctrl.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_flow.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_arp.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_icmp.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ip.h \
+ /usr/include/netinet/in.h /usr/include/sys/socket.h \
+ /usr/include/sys/uio.h /usr/include/bits/uio.h \
+ /usr/include/bits/socket.h /usr/include/bits/socket_type.h \
+ /usr/include/bits/sockaddr.h /usr/include/asm/socket.h \
+ /usr/include/asm-generic/socket.h /usr/include/asm/sockios.h \
+ /usr/include/asm-generic/sockios.h /usr/include/bits/in.h \
+ /usr/include/netinet/ip.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_sctp.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_tcp.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_udp.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_esp.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_higig.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_mbuf_dyn.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_ethdev_core.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_hash.h \
+ /home/chanawat/dpdk-20.05/x86_x64-native-linux-gcc/include/rte_jhash.h
