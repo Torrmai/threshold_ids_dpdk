a72b75ef8870d9c865554cd925bd797cf34903a5
Torrmai <chanawatkuku@gmail.com>
2020-10-30 11:24:59 +0700

    run.sh can do all current options

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-echo "Please enter time peroid in second(integer) [ENTER]: "
-read test 
-./build/senior_backend -l 1-3 -- -T "$test" "-V 1"
+echo -n "Please enter time peroid in second(integer): "
+read tim
+echo -n "Do you want to verbose(Y/n): "
+read ver
+# toggle verbose mode 
+if [ "$ver" == "Y" ]; then 
+	./build/senior_backend -l 1-3 -- -T "$tim" -V 1
+elif [ "$ver" == "y" ]; then
+	./build/senior_backend -l 1-3 -- -T "$tim" -V 1
+else
+	./build/senior_backend -l 1-3 -- -T "$tim"
+fi
